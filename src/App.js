14553665e23de87d75ced33bed653741f6aaf0ab
Chrissy-coder <christine.soropa@gmail.com>
2023-10-15 16:05:17 +1300

    Added searchbar and eventlistener.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div classname="App">
-      <h1>Dictionary App</h1>
-      <div>
-        <footer>
-          Coded by Christine Soropa on {""}
-          <a
-            href="https://github.com/Chrissy-coder/dictionary-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github{" "}
-          </a>
-          and hosted on{" "}
-          <a
-            href="https://tubular-paprenjak-7c66ab.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify
-          </a>
-        </footer>
+      <div className="container">
+        <h1>Dictionary App</h1>
+        <div>
+          <main>
+            <Dictionary />
+          </main>
+          <footer>
+            Coded by Christine Soropa and open-sourced on {""}
+            <a
+              href="https://github.com/Chrissy-coder/dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github{" "}
+            </a>
+            and hosted on{" "}
+            <a
+              href="https://tubular-paprenjak-7c66ab.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
