a70b53c59591ebec6a411aca1d56b3429f1e6575
Chrissy-coder <christine.soropa@gmail.com>
2023-10-15 14:40:00 +1300

    Added workflow links to footer.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hi world</h1>
+    <div classname="App">
+      <h1>Dictionary App</h1>
+      <div>
+        <footer>
+          Coded by Christine Soropa on {""}
+          <a
+            href="https://github.com/Chrissy-coder/dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://tubular-paprenjak-7c66ab.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
