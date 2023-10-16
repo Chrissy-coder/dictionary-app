b0531fd82b0de308882867826fd7cc5a7d455817
Chrissy-coder <christine.soropa@gmail.com>
2023-10-16 19:22:09 +1300

    New Meaning component and render results on page.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function Meaning (props) {
-
+console.log(props.meaning);
+return (
+   <div className="Meaning"> 
+<h3>{props.meaning.partOfSpeech}</h3>
+{props.meaning.definitions.map(function (definition, index)
+{
+  return (
+<div key={index}>
+<p>
+    {props.meaning.definition}
+<br />
+<em>{props.meaning.example}</em>
+</p>
+    </div>
+  );  
+})}
+</div>
+);
 }
