dad3228ed4f2deb6eab0199a6a1219339b7e748e
Chrissy-coder <christine.soropa@gmail.com>
2023-10-20 11:31:35 +1300

    Added phonetic component

---
+++ src/Phonetic.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <span className="text">
+     {props.phonetic.text}
+    </span>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,20 @@
 import React from "react";
-import Meaning from './Meaning';
+import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetic phonetic={Phonetic} />
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-<Meaning meaning={meaning}/>
+              <Meaning meaning={meaning} />
             </div>
-          );  
+          );
         })}
       </div>
     );
