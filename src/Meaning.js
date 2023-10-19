f776226ed4d2cb73cf23116edd8e2201c9ae098a
Chrissy-coder <christine.soropa@gmail.com>
2023-10-20 09:30:47 +1300

    Added definitions and examples  to meaning component.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import Synonyms from "./Synonyms";
 
-export default function Meaning (props) {
-console.log(props.meaning);
-return (
-   <div className="Meaning"> 
-<h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definition}</p>
-        <em>{props.meaning.example}</em>
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <div className="Definition">
+          {" "}
+          <strong>Definition</strong>
+          <br />
+          {props.meaning.definition}
+        </div>
+        <div className="Example">
+          {" "}
+          <strong>Example</strong>
+          <br />
+          {props.meaning.example}
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </div>
       </div>
-    );
-} 
+    </div>
+  );
+}
