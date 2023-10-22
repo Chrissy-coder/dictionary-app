b9c9393d3c1dc430e50db6ec528a11bc4fc1ce80
Chrissy-coder <christine.soropa@gmail.com>
2023-10-23 08:12:29 +1300

    Added styling.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,44 +1,43 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from './Results';
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
 
-function handleResponse (response) {
- setResults(response.data);
-}
+  function handleResponse(response) {
+    setResults(response.data);
+  }
 
-  
   function search(event) {
     event.preventDefault();
-    
 
-    let apiKey = "8f63022fa4b60b22c3t7fe8f63b45ob9"
+    let apiKey = "8f63022fa4b60b22c3t7fe8f63b45ob9";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
-  
 
- function handleKeywordChange(event) {
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-    return (
-      <div className="Dictionary">
-          <form onSubmit={search}>
-            <input
-              type="search"
-              placeholder="Search for a word"
-              autoFocus={true}
-              className="form-control search-input"
-              onChange={handleKeywordChange}
-            />
-          </form>
-          <Results results={results}/>
-      </div>
-    );
-    }
+  return (
+    <div className="Dictionary">
+      <section>
+        <form onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Search for a word"
+            autoFocus={true}
+            className="form-control search-input"
+            onChange={handleKeywordChange}
+          />
+        </form>
+      </section>
+      <Results results={results} />
+    </div>
+  );
+}
