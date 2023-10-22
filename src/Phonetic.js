26c415c9e8ee9432eb39fb9b20df276254f7368f
Chrissy-coder <christine.soropa@gmail.com>
2023-10-23 07:38:53 +1300

    Implemented phonetic component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+
 export default function Phonetic(props) {
-  return (
-    <span className="text">
-     {props.phonetic.text}
-    </span>
-  );
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <em>{props.phonetic}</em>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
