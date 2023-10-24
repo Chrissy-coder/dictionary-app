9433ecc42686f05072da69d006479457de75b45f
Chrissy-coder <christine.soropa@gmail.com>
2023-10-24 15:56:28 +1300

    Implemented photo API and styling.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState([]);
 
   function handleResponse(response) {
     setResults(response.data);
+    let pexelApiKey = "8f63022fa4b60b22c3t7fe8f63b45ob9";
+    let pexelApiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${pexelApiKey}`;
+    axios
+      .get(pexelApiUrl, { headers: { Authorization: `Bearer ${pexelApiKey}` } })
+      .then(handleImages);
+  }
+
+  function handleImages(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -39,9 +50,10 @@ export default function Dictionary() {
             onChange={handleKeywordChange}
           />
         </form>
-        <div className="hint">Suggested words: sunset, wine, yoga</div>
+        <div className="hint">Suggested words: sunset, book, horizon</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
